a0806986db8b4eb327b4e159b743fae4fad6de77
Manthan <kharotemanthan@gmail.com>
2026-03-23 12:13:35 +0530

    added Pricing option

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import PricingPage from './components/PricingPage';
+import CheckoutPage from './components/CheckoutPage';
+import SuccessPage from './components/SuccessPage';
 
 function App() {
   return (
     <MainLayout>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/success" element={<SuccessPage />} />
+      </Routes>
     </MainLayout>
   );
 }
